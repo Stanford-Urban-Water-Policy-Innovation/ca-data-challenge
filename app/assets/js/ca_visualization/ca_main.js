4d276937618b9787517ca9ae1bfb7483ef3a1cff
Patricia Gonzales <patricia@DN0a22b31d.SUNet>
2016-12-08 15:51:52 -0800

    ca water data challenge entry

--- app/assets/js/ca_visualization/ca_main.js
+++ app/assets/js/ca_visualization/ca_main.js
@@ -18,20 +18,14 @@ d3.json("https://api.myjson.com/bins/3fizm", function(remote_json){
  	var region2 = cf.dimension(function(d){return d.hydrologic_region; });
 	var conservation_taf = cf.dimension(function(d){return d.conservation/325851000; });
   	var conservation_per = cf.dimension(function(d){return Math.ceil(d.percent /0.05) * 0.05 * 100; });
-  	var gpcd = cf.dimension(function(d){return Math.ceil((d.conservation + d.production_2013)/d.population/365/10) * 10; });
-  	var rgpcd = cf.dimension(function(d){return Math.ceil(d.residential/d.population/365/10) * 10; })
-  	var residential = cf.dimension(function(d){return Math.ceil(d.residential/(d.conservation + d.production_2013)/0.05) * 0.05 * 100; });
-  
+
   // ------------------------------------------------------------------
   // build groups
   
   	var all = cf.groupAll();  
-  	var region_suppliers_count = region.group().reduceCount();
+  	var region_suppliers_count = region2.group().reduceCount();
   	var conservation_taf_sum = region.group().reduceSum(function(d) {return d.conservation/325851000; }); 
   	var conservation_per_group = conservation_per.group();
-  	var gpcd_group = gpcd.group();
-  	var rgpcd_group = rgpcd.group();
-  	var residential_group = residential.group();
   	
   // ------------------------------------------------------------------
   // unique name extraction
@@ -80,39 +74,6 @@ d3.json("https://api.myjson.com/bins/3fizm", function(remote_json){
     	.xUnits(dc.units.fp.precision(5))
     	.elasticY(true)
     	.yAxisLabel("number of suppliers")
-    	.xAxisLabel("%");  
-  
-  	var gpcd_chart = dc
-  		.barChart("#gpcd_chart")
-    	.width(400)
-    	.height(200)
-    	.dimension(gpcd)
-    	.group(gpcd_group)
-    	.x(d3.scale.linear().domain([0,1000]))
-    	.xUnits(dc.units.fp.precision(10))
-    	.yAxisLabel("number of suppliers")
-    	.xAxisLabel("gallons per capita per day");
-    
-  	var rgpcd_chart = dc
-  		.barChart("#rgpcd_chart")
-    	.width(400)
-    	.height(200)
-    	.dimension(rgpcd)
-    	.group(rgpcd_group)
-    	.x(d3.scale.linear().domain([0,1000]))
-    	.xUnits(dc.units.fp.precision(10))
-    	.yAxisLabel("number of suppliers")
-    	.xAxisLabel("gallons per capita per day");
-   
-  	var residential_chart = dc
-  		.barChart("#residential_chart")
-    	.width(400)
-    	.height(200)
-    	.dimension(residential)
-    	.group(residential_group)
-    	.x(d3.scale.linear().domain([0,100]))
-    	.xUnits(dc.units.fp.precision(5))
-    	.yAxisLabel("number of suppliers")
     	.xAxisLabel("%");
 
    
@@ -130,9 +91,6 @@ d3.json("https://api.myjson.com/bins/3fizm", function(remote_json){
     	|| suppliers_chart.filters().length >0
       || aggregate_chart.filters().length >0
       || percent_chart.filters().length >0
-      || gpcd_chart.filters().length >0
-      || rgpcd_chart.filters().length >0
-      || residential_chart.filters().length >0
 				){
         d3.select("#dcjs_reset_btn")
         	.remove();
@@ -146,9 +104,6 @@ d3.json("https://api.myjson.com/bins/3fizm", function(remote_json){
               suppliers_chart.filter(null);
               aggregate_chart.filter(null);
               percent_chart.filter(null);
-              gpcd_chart.filter(null);
-              rgpcd_chart.filter(null);
-             	residential_chart.filter(null);
               dc.redrawAll();
           });
     
@@ -164,7 +119,4 @@ d3.json("https://api.myjson.com/bins/3fizm", function(remote_json){
   suppliers_chart.on('filtered', function(){showButton();});
   aggregate_chart.on('filtered', function(){showButton();});
   percent_chart.on('filtered', function(){showButton();});
-  gpcd_chart.on('filtered', function(){showButton();});  
-  rgpcd_chart.on('filtered', function(){showButton();});
-  residential_chart.on('filtered', function(){showButton();});
 });
